6e9d252d9f1b8613fefc0e0c2fe5e79bc459ecbf
Dmitriy V Kozubskiy <kozubskiyd@vezubr.ru>
2019-10-05 02:54:40 +0700

    declarations...

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,6 @@
-export declare function implementsInterface<T>(object: {[index: string]: unknown}|any, objInterface: {[index: string]: any;}): object is T;
+export declare function implementsInterface<T>(object: {
+    [index: string]: unknown;
+} | any, objInterface: {
+    [index: string]: any;
+}): object is T;
 export declare function arrayMatchesInterface(actualArray: unknown[], allowedTypesForArray: unknown[]): boolean;
